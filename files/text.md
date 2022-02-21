7242368022a0b4773fc66ecaf9ebcb1231131809
hendrix <leandrohendrix@gmail.com>
2022-02-21 06:39:59 -0300

    Fs -> RedFile

---
+++ files/text.md
@@ -0,0 +1,4 @@
+My first company was Gimba, the website is www.gimba.com.br.
+My second company was Totvs, the website is www.totvs.com.br.
+My thrid company was CityHall of SP, the website is www.prefeiturasp.com.br
+
--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const chalk = require('chalk');
+class Index {
+    constructor() {
+        this._chalk = require('chalk');
+        this._fs = require('fs');
+    }
 
-console.log(chalk.red("Welcome to nodeJs"));
+    redFile(path) {
+        const enconding = "utf-8";
+        //_ dentro da função ignora o valor do primeiro parâmetro
+        this._fs.readFile(path, enconding, (_, text) => {
+            console.log(this._chalk.green(text));
+        });
+    }
+}
+
+let index = new Index();
+index.redFile('./files/text.md');
+
