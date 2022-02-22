1cd3c85163fe06abfb18960c0aed3e6f1f3145e8
hendrix <leandrohendrix@gmail.com>
2022-02-22 20:08:01 -0300

    Throw Error

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ class Index {
         this._chalk = require('chalk');        
     }
     redFile() {
-        //_ dentro da função ignora o valor do primeiro parâmetro
-        this._fs.readFile(this._path, this._enconding, (_, fileText) => {
+        this._fs.readFile(this._path, this._enconding, (error, fileText) => {
+            if (error)
+              throw new Error(this._chalk.red(error.code, 'File not found'));
             console.log(this._chalk.green(fileText));
         });
     }
 }
 
-let index = new Index('./app/files/text.md');
+let index = new Index('./app/files/tex.md');
 index.redFile();
 
