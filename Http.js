7fb34012964bccea28325e33cdadb72e2f88f445
hendrix <leandrohendrix@gmail.com>
2022-02-28 16:35:42 -0300

    Http

---
+++ Http.js
@@ -0,0 +1,8 @@
+class Http {
+   
+   static lookUpLinks(links){
+      console.log('CheckLinks', links); 
+   }    
+}
+
+module.exports = Http;
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const File = require('./File.js');
+const Http = require('./Http.js');
+const path = process.argv; //Obtém informações do terminal
 
-async function main(){
-   const path = process.argv;
+async function main() {
    let file = new File(path[2]);
    let links = await file.readFile_2();
 
-   console.log('Links', links);
+   if (path[3] == 'validar') {
+      Http.lookUpLinks(links);
+   } else {
+      console.log('Links', links);
+   }
 }
 
 main();
