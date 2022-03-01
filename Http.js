076ae6f31c57bc481e501eb0d597488263a30dd3
hendrix <leandrohendrix@gmail.com>
2022-03-01 10:31:35 -0300

    Feth

--- Http.js
+++ Http.js
@@ -1,11 +1,27 @@
 class Http {
    
-   lookUpLinks(links){
-      console.log(this.getUrl(links));
+   constructor(){
+      this._fetch = require('node-fetch');
+   }
+
+   async lookUpLinks(data){
+      let links = this.getUrl(data);
+      let status = await  this.lookUpStatus(links);
+      console.log(status);
    } 
    
    getUrl(links){
       return links.map(links => Object.values(links).join());   
+   }
+
+   async lookUpStatus(links){
+      const statusArr = Promise.all(
+         links.map(async url  => {
+            const res = await this._fetch(url);
+            return res.status;   
+         })
+      );
+      return statusArr;      
    }
 }
 
