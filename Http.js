d47b745f14cf26fc67e3ffc49cdef0be875f8841
hendrix <leandrohendrix@gmail.com>
2022-03-03 08:03:28 -0300

    Handle_Error

--- Http.js
+++ Http.js
@@ -1,28 +1,36 @@
 class Http {
-   
-   constructor(){
+
+   constructor() {
       this._fetch = require('node-fetch');
    }
 
-   async lookUpLinks(data){
+   async lookUpLinks(data) {
       let links = this.getUrl(data);
-      let status = await  this.lookUpStatus(links);
-      
+      let status = await this.lookUpStatus(links);
+
       console.log(status);
-   } 
-   
-   getUrl(links){
-      return links.map(links => Object.values(links).join());   
    }
 
-   async lookUpStatus(links){
-      const statusArr = Promise.all(
-         links.map(async url  => {
-            const res = await this._fetch(url);
-            return res.status;   
-         })
-      );
-      return statusArr;      
+   getUrl(links) {
+      return links.map(links => Object.values(links).join());
+   }
+
+   async lookUpStatus(links) {
+      try {
+         const statusArr = Promise.all(
+            links.map(async url => {
+               const res = await this._fetch(url);
+               return res.status;
+            })
+         );
+         return statusArr;
+      } catch (error) {
+         this.handleError(error);
+      }
+   }
+
+   handleError(error) {
+      throw new Error(`Error:${error}`);
    }
 }
 
